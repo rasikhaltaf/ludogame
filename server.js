379aa6098682d8fa50d417b4a923403b1db72066
unknown <rasikhaltaf2000@gmail.com>
2023-03-10 16:34:32 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+    
+    ludogame = () =>{
+    const play1 = Math.floor(Math.random() *6) + 1;
+    const play1dice = `images/img${play1}.png`;
+    document.getElementById('check1').setAttribute('src', play1dice);
+
+    const play2 = Math.floor(Math.random() *6) + 1;
+    const play2dice = `images/img${play2}.png`;
+    document.getElementById('check2').setAttribute('src', play2dice);
+
+    if(play1 > play2){
+        document.querySelector('h1').innerHTML = "Player 1 won :)";
+    }else
+    if(play2 > play1){
+        document.querySelector('h1').innerHTML = "Player 2 won :)";
+    }
+    else{
+        document.querySelector('h1').innerHTML ="DRAW";
+    }
+}
